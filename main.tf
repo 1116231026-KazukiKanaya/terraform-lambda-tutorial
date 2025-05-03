8b7afbcd8eaf366fcd71fd1ce871029047faceee
Kazuki Kanaya Desktop <k.melon0106@gmail.com>
2025-05-04 03:12:02 +0900

    fmtの適用

--- main.tf
+++ main.tf
@@ -5,8 +5,8 @@ terraform {
   required_version = ">= 1.5.0"
   required_providers {
     aws = {
-        source = "hashicorp/aws"
-        version = "~> 5.0"
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
   }
 }
@@ -16,7 +16,7 @@ terraform {
 # ========================================
 provider "aws" {
   profile = "terraform"
-  region = "ap-northeast-3"
+  region  = "ap-northeast-3"
 }
 
 # ========================================
